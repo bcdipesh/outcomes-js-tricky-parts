2768b4222ce0e3c7e7a631efcedbb6156c67b88d
Dipesh B C <bcdipeshwork@gmail.com>
2024-01-25 10:47:36 -0500

    Implement curried-add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,20 @@
 function curriedAdd(total) {
+  let sum = total;
 
+  function add(num) {
+    if (num) {
+      sum = sum + num;
+      return add;
+    }
+
+    return sum;
+  }
+
+  if (!total) {
+    return 0;
+  }
+
+  return add;
 }
 
 module.exports = { curriedAdd };
--- curried-add.test.js
+++ curried-add.test.js
@@ -1,15 +1,15 @@
-const { curriedAdd } = require("./curried-add");
+const { curriedAdd } = require('./curried-add');
 
-describe("curriedAdd", function() {
-  it("returns 0 when called with no arguments", function() {
+describe('curriedAdd', function () {
+  it('returns 0 when called with no arguments', function () {
     expect(curriedAdd()).toBe(0);
   });
 
-  it("returns a function when called with an argument", function() {
+  it('returns a function when called with an argument', function () {
     expect(curriedAdd(3)).toBeInstanceOf(Function);
   });
 
-  it("accumulates a total with repeated function calls", function() {
+  it('accumulates a total with repeated function calls', function () {
     expect(curriedAdd(1)()).toBe(1);
     expect(curriedAdd(1)(2)()).toBe(3);
     expect(curriedAdd(1)(2)(3)()).toBe(6);
